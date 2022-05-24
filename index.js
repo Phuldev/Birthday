d10388b8a7b862a5e9a0f5fe3532d26f683d3c7b
Phuldev Mandal <59725190+Phuldev@users.noreply.github.com>
2022-05-24 23:03:57 +0545

    Update index.js
    right date

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const giftbox = document.getElementById('birtwrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  // birthdate: 'July,8, 2022',
-  birthdate: 'May,15, 2022',
+  birthdate: 'July,8, 2022',
   name: 'Dear Garima'
 };
 
